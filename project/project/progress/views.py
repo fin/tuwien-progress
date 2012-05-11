6288464ceceb5e121e590dba5adec68fd903ae73
fin <fin+github@xbhd.org>
2012-05-11 18:02:43 +0200

    view stub

--- project/project/progress/views.py
+++ project/project/progress/views.py
@@ -4,12 +4,14 @@ from models import Certificate
 import json
 
 def has_cert(request):
+    response = HttpResponse()
     try:
         certlist = request.POST['certs']
         certlist = json.loads(certlist)
 
         for certdata in certlist:
             cert = Certificate.objects.create(**certdata)
+        response.body = 'ok'
     except Exception, e:
-        response.data = 'lol'
+        response.body = 'lol'
     return response
