0b036c124cc4e9398b7ad482b473ad9b5080d437
fin <fin+github@xbhd.org>
2012-05-30 17:20:14 +0200

    exception handling

--- project/project/progress/views.py
+++ project/project/progress/views.py
@@ -14,20 +14,24 @@ def has_cert(request):
     certlist = request.POST.get('certs',None)
     user = request.user
 
-    if request.GET.get('auth_token'):
-        user = Profile.objects.get(auth_key=request.GET['auth_token']).user
+    try:
+        if request.GET.get('auth_token'):
+            user = Profile.objects.get(auth_key=request.GET['auth_token']).user
 
-    if user.is_anonymous():
-        response.content = 'not logged in!'
-    elif certlist:
-        certlist = json.loads(certlist)
+        if user.is_anonymous():
+            response.content = 'not logged in!'
+        elif certlist:
+            certlist = json.loads(certlist)
 
-        for certdata in certlist:
-            if not Certificate.objects.find(user=user, **certdata):
-                cert = Certificate.objects.create(user=user,**certdata)
-        response.content = 'ok'
-    else:
-        response.content = json.dumps([x.__dict__ for x in Certificate.objects.filter(user=user)])
+            for certdata in certlist:
+                if not Certificate.objects.find(user=user, **certdata):
+                    cert = Certificate.objects.create(user=user,**certdata)
+            response.content = 'ok'
+        else:
+            response.content = json.dumps([x.__dict__ for x in Certificate.objects.filter(user=user)])
+    except Exception, e:
+        import traceback
+        e.content = str(e) + '\n' + traceback.format_exc()
     response['Access-Control-Allow-Origin'] = '*'
     return response
 
