4bd43a955cc423ba17eb92bf623c7ca531634771
fin <fin+github@xbhd.org>
2012-05-30 13:46:15 +0200

    add auth key to user profile model

--- project/project/tuprofile/models.py
+++ project/project/tuprofile/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from userena.models import UserenaBaseProfile
+import uuid
 
 class Profile(UserenaBaseProfile):
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=('user'),
                                 related_name='my_profile')
+    auth_key = models.CharField(max_length=255, unique=True)
+
+    def safe(self, *args, **kwargs):
+        if not self.auth_key:
+            self.auth_key = str(uuid.uuid4())
+        super(Profile, self).save()
 
